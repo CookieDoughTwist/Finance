57a00215f6f7bf5b595a545395ca425bb61ed4c7
CookieDoughTwist <andrew01810@gmail.com>
2017-12-03 23:20:01 -0500

    Initial gdax_fetcher.py which polls crypto data from GDAX. Threshold alarms implemented.

---
+++ DataStructures.py
@@ -0,0 +1,56 @@
+#import pandas
+import sys
+import numpy
+
+class CircularQueue:
+
+    i_0 = 0
+    k = 0
+    
+    def __init__(self,n=4,default=0):        
+        self.n = n
+        self.arr = [default]*n    
+        
+    def push(self,value):
+        cur_end = (self.i_0+self.k)%self.n
+        self.arr[cur_end] = value
+        if self.k < self.n:
+            self.k += 1
+        elif cur_end is self.i_0:
+            self.i_0 = (self.i_0+1)%self.n
+        
+    def get(self,idx):
+        return self.arr[(self.i_0+idx)%self.n]
+    
+    def mean(self):
+        return numpy.sum(self.arr)/float(self.k)        
+    
+    def __str__(self):
+        out = "n=%d: [" % self.n        
+        for i in range(self.k):                     
+            out += "%f, " % self.get(i)            
+        out += "\b\b]"
+        return out
+    
+        
+def main():
+    sys.stdout.write("Testing DataStructures\n")
+    sys.stdout.flush()
+    dm = CircularQueue()
+    dm.push(1)
+    print dm.mean()
+    dm.push(2)
+    print dm.mean()
+    dm.push(3)
+    print dm.mean()
+    dm.push(4)
+    print dm.mean()
+    dm.push(5)
+    print dm.mean()
+    dm.push(6)
+    print dm.mean()
+
+    
+
+if __name__ == "__main__":
+    main()
---
+++ gdax_fetcher.py
@@ -0,0 +1,55 @@
+from datetime import datetime, timedelta
+
+import requests
+import time
+import datetime
+import winsound
+import sys
+import DataStructures
+
+
+crypto_symbols = ('BTC','ETH','LTC')
+warning_tolerance = (10000,425,90)
+tol_frac = .01
+frequency = 2500
+duration = 1000
+tol_dict = dict(zip(crypto_symbols,warning_tolerance))
+sys.stdout.write("%s\n\n" % tol_dict)
+url_dict = dict()
+window_dict = dict()
+
+
+for sym in crypto_symbols:
+    url_dict[sym] = 'https://api.gdax.com/products/%s-USD/ticker' % sym
+    window_dict[sym] = DataStructures.CircularQueue(60)
+    
+
+
+def main():
+    while True:
+        sys.stdout.write("%s     " % datetime.datetime.now())
+        for sym in crypto_symbols:         
+            url = url_dict[sym]
+            # Query CoinDesk Current price API
+            response = requests.get(url).json()            
+            try:
+                price = round(float(response['bid']), 2)                
+                window_dict[sym].push(price)                
+                cur_mean = window_dict[sym].mean()
+                dev_frac = (price-cur_mean)/cur_mean
+                if price < tol_dict[sym] or dev_frac > tol_frac or dev_frac < -tol_frac:
+                    winsound.Beep(frequency, duration)
+            except:
+                price = 0
+                dev_frac = 0
+            
+            sys.stdout.write("%s: $%.2f" % (sym,price))
+            sys.stdout.write("(%.2f%%)" % (dev_frac*100))
+            sys.stdout.write(", ")
+        sys.stdout.write("\n");
+        sys.stdout.flush()
+
+        time.sleep(10)
+
+if __name__ == "__main__":
+    main()
