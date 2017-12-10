53b05ae3c3b801a591c2837cba5d858c714c2e1d
CookieDoughTwist <andrew01810@gmail.com>
2017-12-10 01:09:52 -0500

    Progress on storage structures for holdings and orders.

--- Robinhood.py
+++ Robinhood.py
@@ -1,6 +1,5 @@
 import sys
 import requests
-import urllib
 
 URL_DICT = {
     "login": "https://api.robinhood.com/api-token-auth/",
@@ -30,6 +29,23 @@ URL_DICT = {
     "fundamentals": "https://api.robinhood.com/fundamentals/",
 }
 
+class Order:
+    def __init__(self,data_dict):
+        self.load(data_dict)
+        
+    def load(self,data_dict):
+        self.id = data_dict['id']
+        self.updated_at = data_dict['updated_at']
+        self.instrument = data_dict['instrument']
+        self.state = data_dict['state']
+        self.type = data_dict['type']
+        
+    def update(self,data_dict):        
+        if self.updated_at is not data_dict['updated_at']:
+            self.load(data_dict)
+            return True
+        return False
+        
 class Robinhood:
 
     header_dict = {
@@ -70,13 +86,24 @@ class Robinhood:
         except:
             sys.stdout.write("Logout error!\n")
         return req
-        
-    def get_account(self):
+    
+    def raise_session(self,url,payload=None):
+        if payload is None:
+            res = self.session.get(url)
+        else:
+            res = self.session.get(url,payload)
+        res = res.json()
+        return res
+    
+    def get_account(self,type=None):
         """ Fetch account information """
         res = self.session.get(URL_DICT['accounts'])
         res.raise_for_status()  #auth required
         res = res.json()
-        return res['results'][0]
+        if type is None:
+            return res['results'][0]
+        else:
+            return res['results'][0][type]
         
     def investment_profile(self):
         """ Fetch investment_profile """
@@ -104,6 +131,87 @@ class Robinhood:
         except:
             return None
     
+    def positions(self):
+        return self.session.get(self.endpoints['positions']).json()
+
+    def securities_owned(self):
+        return self.session.get(URL_DICT['positions']+'?nonzero=true').json()        
+    
+    def portfolios(self,type=None):
+        """ type = 'adjusted_equity_previous_close' / 'equity' / 'equity_previous_close' /
+                   'excess_margin' / 'extended_hours_equity' / 'extended_hours_market_value' /
+                   'last_core_equity' / 'last_core_market_value' / 'market_value' """
+        req = self.session.get(URL_DICT['portfolios'])
+        req.raise_for_status()
+        if type is None:
+            return req.json()['results'][0]
+        else:
+            return req.json()['results'][0][type]
+    
+    def last_Order(self):
+        order_list = self.list_order_ids()
+        return Order(self.order_details(order_list[0]))
+    
+    def order_history(self,date=None,sym=None):
+        payload = dict()
+        if date is not None:
+            payload['updated_at'] = date # ex: date = '2017-08-15'
+        if sym is not None:
+            instrument = self.instruments(sym)[0]
+            payload['instrument'] = instrument['url']
+        print payload
+        return self.session.get(URL_DICT['orders'],data=payload).json()
+
+    def list_order_ids(self):
+        ''' returns a list of all order_IDs, ordered from newest to oldest '''
+        res = self.session.get(URL_DICT['orders'])
+        if res.status_code == 200:
+            orders = []
+            for i in res.json()['results']:
+                URL = i['url']
+                orders.append(URL[URL.index("orders")+7:-1])
+            return orders
+        else:
+            raise Exception("Could not retrieve orders: " + res.text)
+    
+    def list_orders(self):
+        order_ids = self.list_order_ids()
+        order_list = list()
+        for order_id in order_ids:
+            order_list.append(Order(self.order_details(order_id)))
+        return order_list
+    
+    def order_details(self, order_ID):
+        ''' Returns an order object which contains information about an order 
+        and its status'''
+        res = self.session.get(URL_DICT['orders'] + order_ID + "/")
+        if res.status_code == 200:
+            return res.json()
+        else:
+            raise Exception("Could not get order status: " + res.text)
+    
+    def order_status(self, order_ID):
+        ''' Returns an order status string'''
+        return self.order_details(order_ID)['state']
+    
+    def list_order_details(self):
+        ''' Generates a dictionary where keys are order_IDs and values are 
+        order objects. '''
+        detailed_orders = {}
+        for i in self.list_order_ids():
+            order = self.order_details(i)
+            order['symbol'] = self.session.get(order['instrument']).json()['symbol']
+            detailed_orders[i] = order
+        return detailed_orders
+    
+    def cancel_order(self, order_ID):
+        ''' Cancels order with order_ID'''
+        res = self.session.post(URL_DICT['orders'] + order_ID + "/cancel/")
+        if res.status_code == 200:
+            return res
+        else:
+            raise Exception("Could not cancel order: " + res.text)
+    
     def place_order(self,instrument,quantity=1,bid_price=0.0,transaction=None,
                     trigger='immediate',order='market',time_in_force = 'gfd'):        
         """ Place an order with Robinhood """
@@ -120,15 +228,8 @@ class Robinhood:
             'trigger': trigger,
             'type': order.lower()
         }
-        tries = 0
-        while tries < 3:
-            try:
-                res = self.session.post(URL_DICT['orders'],data=payload)
-                res.raise_for_status()
-                break
-            except:
-                sys.stdout.write("Order placement failed!\n")
-                tries += 1                            
+        res = self.session.post(URL_DICT['orders'],data=payload)
+        res.raise_for_status()                          
         return res
         
     def limit_buy(self,sym,quantity=1,bid_price=0.0):
--- Trader.py
+++ Trader.py
@@ -6,6 +6,12 @@ import Queue
 import DataStructures
 import Robinhood
 
+class TickerTracker:
+    
+    def __init__(self,sym,instrument):
+        self.sym = sym
+        self.instrument = instrument
+
 class RobinhoodTrader:
 
     active = False
@@ -16,6 +22,7 @@ class RobinhoodTrader:
         self.log_loc = log_loc
         self.rb = Robinhood.Robinhood()
         self.login()
+        self.trackers = dict()
     
     def login(self):
         f = open(self.login_path,'r')
@@ -23,12 +30,38 @@ class RobinhoodTrader:
         pw = f.readline().strip()
         f.close()
         self.active = self.rb.login(us,pw)        
-    
+          
+    def load(self):
+        results = self.rb.securities_owned()['results']
+        for holding in results:
+            instrument = self.rb.raise_session(holding['instrument'])
+            tracker = TickerTracker(instrument['symbol'],instrument)
+            self.trackers[tracker.sym] = tracker
         
-    def update(self):
-        print self.rb.get_price('AAPL')
-        self.rb.limit_sell('AAPL',10,1000.2)
+    def load_active_orders(self):        
+        orders = self.rb.list_orders()
+        for order in orders:
+            print order.state
+        
+            
+    def update(self):      
+        start = time.time()
+        #self.rb.order_history('2017-12-08')
+        #listoforders = self.rb.list_orders() #all the orders recent to old
+        #print self.rb.order_details(listoforders[len(listoforders)-1])['instrument'].json()['symbol']
+        #firstorder = Robinhood.Order(listoforders[0]) #most recent order
+        #firstorder.id
+        #print self.rb.list_order_details()
+        #print self.rb.order_status(firstorder) #prints "cancelled" etc
+        #self.load_active_orders()
+        self.load()
+        print self.trackers
+        
+        
+        end = time.time()
+        sys.stdout.write("Update time: %f\n" % (end - start))
         sys.stdout.flush()
+        
 
 def main():
     sys.stdout.write("Hajimeyou!\n")
@@ -39,6 +72,9 @@ def main():
         login_path = sys.argv[1]
     
     td = RobinhoodTrader(login_path)
+    #td.load(['AAPL','TSLA','F'])
+    #td.load_active_orders()
+    
     td.update()
     #while True:
         #td.update()
